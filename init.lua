3f6d62315a956bb2835b155fff0247ee937db1a4
Michael R. Taylor <michael@atomic.city>
2021-09-11 00:55:19 -0500

    Made plugin system a bit more robust including a modular config system for plugins

--- init.lua
+++ init.lua
@@ -3,3 +3,15 @@ require('plugins')
 
 -- Set colorscheme
 vim.cmd('colorscheme monokai')
+
+-- Set font
+vim.o.guifont = 'Hack NF:h10'
+
+-- Set default encoding
+vim.o.encoding = 'UTF-8'
+
+-- Set vertical column guides
+vim.o.colorcolumn = '100,120'
+
+-- Show whitespace
+vim.o.list = true
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,5 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Use packer.nvim for plugin management
+local packer = require('packer')
 
 -- Install the plugin manager if it hasn't been installed already
 local first_time_install = false
@@ -10,15 +11,50 @@ if fn.empty(fn.glob(install_path)) > 0 then
     first_time_install = true
 end
 
-require('packer').startup(function()
-                                 -- Packer can manage itself
-                                 use 'wbthomason/packer.nvim'
+-- Setup plugins here
+packer.startup(function()
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-                                 -- Color schemes
-                                 use 'sickill/vim-monokai'
+    -- Color schemes
+    use 'sickill/vim-monokai'
+
+    -- UI
+    use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+    use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
+    use 'mhinz/vim-startify'
+    use 'folke/which-key.nvim'
+
+    -- Searching
+    use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
 end)
 
--- Install plugins if this is the first time install
-if first_time_install then
-    vim.cmd 'PackerSync'
+-- Add an event handler for PackerComplete to run configure_plugins()
+vim.cmd([[
+    augroup packer_post_load
+        autocmd!
+        autocmd User PackerComplete lua configure_plugins()
+    augroup end
+]])
+
+-- Install any plugins that need installed
+-- NOTE:  Sometimes a PackerSync is required.  Look into why.
+packer.install()
+
+-- Helper function used to see if plugin config exists
+local function file_exists(fname)
+    local stat = vim.loop.fs_stat(fname)
+    return (stat and stat.type) or false
+end
+
+-- Function to call configuration scripts for each loaded plugin (needs to be called after
+-- the PackerComplete user event)
+function configure_plugins()
+    for k,v in pairs(_G.packer_plugins) do
+        local safe_name = k:gsub('%.', '_')
+        local plugin_cfg = fn.stdpath('config')..'/lua/config/'..safe_name..'.lua'
+        if v.loaded and file_exists(plugin_cfg) then
+            require('config.'..safe_name)
+        end
+    end
 end
