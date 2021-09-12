2e2cfed9cd63544c3233cf4fbde68255cedf54b0
Michael R. Taylor <michael@atomic.city>
2021-09-12 00:16:08 -0500

    Ported previous init.vim settings to init.lua and changed theme to Tokyo Night

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ require('homescreen')
 require('plugins')
 
 -- Set colorscheme
-vim.cmd('colorscheme monokai')
+vim.cmd[[colorscheme tokyonight]]
 
 -- Set font
 vim.o.guifont = 'Hack NF:h10'
@@ -17,11 +17,38 @@ vim.o.guifont = 'Hack NF:h10'
 -- Set default encoding
 vim.o.encoding = 'UTF-8'
 
+-- Setup copy/paste to work in Windows
+--vim.o.clipboard:prepend { 'unnamed', 'unnamedplus' }
+vim.cmd('set clipboard^=unnamed,unnamedplus')
+
 -- Set vertical column guides
 vim.o.colorcolumn = '100,120'
 
+-- Set default font behavior
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.smarttab = true
+vim.o.autoindent = true
+vim.o.expandtab = true
+
+-- Ignore octal number format
+--vim.o.nrformats:remove { 'octal' }
+vim.cmd('set nrformats-=octal')
+
 -- Show whitespace
+vim.o.listchars = 'tab:»\\ ,extends:›,precedes:‹,nbsp:·,trail:·'
 vim.o.list = true
+
+-- Set line numbering
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Set timeout for which-key
 vim.o.timeoutlen = 500
+
+-- Use 'jk' instead of ESC in insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<esc>', {noremap = true})
+
+-- Setup key mappings
+require('keymap')
