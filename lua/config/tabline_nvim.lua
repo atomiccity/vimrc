89c2836d444926646628a5df79a3e30737cf3953
Michael R. Taylor <michael@atomic.city>
2021-09-14 00:20:03 -0500

    Configure tabline to show buffer number

--- lua/config/tabline_nvim.lua
+++ lua/config/tabline_nvim.lua
@@ -1,10 +1,11 @@
 require('tabline').setup {
-	options = {
-		always_show_tabs = true
-	}
+    options = {
+        always_show_tabs = true,
+        tabline_show_bufnr = true
+    }
 }
 
 vim.cmd[[
-	set guioptions-=e " Use showtabline in gui vim
-	set sessionoptions+=tabpages,globals " store tabpages and globals in session
+    set guioptions-=e " Use showtabline in gui vim
+    set sessionoptions+=tabpages,globals " store tabpages and globals in session
 ]]
